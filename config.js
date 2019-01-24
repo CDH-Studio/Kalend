b4804311d15a45a5a41187921fd85a8d317e4b5b
Benoît Jeaurond <jeaurond.benoit@gmail.com>
2019-01-24 13:42:30 -0500

    Added Google Sign-In fonctionnalities

---
+++ config.js
@@ -0,0 +1,3 @@
+export const webClientId = "359972006564-9tngmtaigit300v7kqjr48sph3apvq38.apps.googleusercontent.com";
+
+export const googleIdentityScope = ["profile", "https://www.googleapis.com/auth/calendar"];
