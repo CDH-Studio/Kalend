2d216d83e0610e5f293235b3147462640e58b90e
Benoît Jeaurond <jeaurond.benoit@gmail.com>
2019-01-28 11:55:12 -0500

    Fixed Jest issue

--- __tests__/event-test.js
+++ __tests__/event-test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Event from '../src/components/Event';
-import renderer from 'react-test-renderer';
+import React from "react";
+import Event from "../src/components/Event";
+import renderer from "react-test-renderer";
 
-test('Event renders correctly', () => {
-    const tree = renderer.create(<Event/>).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+test("Event renders correctly", () => {
+  const tree = renderer.create(<Event />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
