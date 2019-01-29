6fb1b1b8d842592fa8f12a35193d266e73ccf76e
Fahad Hayat <fahadhayat@outlook.com>
2019-01-29 15:55:04 -0500

    Some GOOOOD STUFF

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,9 @@ router.get("/", function(req, res, next){
 	res.render("index.html")
 }); 
 
+router.get("/api/test", function(req, res, next){
+	let data = {test: "This is test data"};
+	res.send(data);
+});
+
 module.exports = router;
