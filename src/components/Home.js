6c3b81221164052825e4ad3d2f232644cfad4d85
MarieEveLegault <marie_legault_@hotmail.com>
2019-01-30 09:45:14 -0500

    Clean Home.js, added iOS icons and app name.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { ImageBackground, StatusBar, StyleSheet, Button, View, Image, Text } from 'react-native';
+import {ImageBackground, StatusBar, StyleSheet, View, Image, Text} from 'react-native';
 import {GoogleSigninButton} from 'react-native-google-signin';
-import { googleSignIn, googleSignOut, googleRevokeAccess, googleIsSignedIn, googleGetCurrentUserInfo } from '../services/google_identity';
+import {googleSignIn} from '../services/google_identity';
 import LinearGradient from 'react-native-linear-gradient';
 
 
@@ -18,23 +17,19 @@ class Home extends React.Component {
 	render() {
 		return (
 			<LinearGradient style={styles.container} colors={['#1473E6', '#0E55AA']}>
-				<ImageBackground  style={styles.container} source={require('../assets/img/pattern.png')} resizeMode="repeat">
+				<ImageBackground  style={styles.container} source={require('../assets/img/loginScreen/backPattern.png')} resizeMode="repeat">
 			
 					<StatusBar translucent={true} backgroundColor={'#00000050'}/>
 				
-					<View style={{alignItems:'center', flex:1, flexDirection: 'column', justifyContent: 'space-evenly'}}>
+					<View style={styles.content}>
 						<View>
-							<Image style={{height: 100, width: undefined}} source={require('../assets/img/logoScreen.png')} resizeMode="contain"/>
+							<Image style={styles.logo} source={require('../assets/img/kalendFullLogo.png')} resizeMode="contain"/>
 							<Text style={styles.text}>The Better Way to Start your Month!</Text>
 						</View>
 									
-						<Image style={{height:'35%'}} source={require('../assets/img/userLoginScreen.png')} resizeMode="contain" />
+						<Image style={styles.userIcon} source={require('../assets/img/loginScreen/userIcon.png')} resizeMode="contain" />
 					
-						<GoogleSigninButton
-							style={{width: 312, height: 48}}
-							size={GoogleSigninButton.Size.Wide}
-							color={GoogleSigninButton.Color.Light}
-							onPress={this.signIn} />
+						<GoogleSigninButton style={styles.signInButton} size={GoogleSigninButton.Size.Wide} color={GoogleSigninButton.Color.Light} onPress={this.signIn} />
 					</View>
 					
 				</ImageBackground >
@@ -48,14 +43,35 @@ const styles = StyleSheet.create({
 		flex: 1
 	},
 
+	content: {
+		alignItems: 'center',
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-evenly'
+	},
+
+	logo: {
+		height: 100, 
+		width: undefined
+	},
+
 	text: {
 		fontFamily: 'Raleway-Regular',
-		color:'#FFFFFF',
+		color: '#FFFFFF',
 		fontSize: 20,
 		paddingTop: 10,
 		textShadowColor: 'rgba(0, 0, 0, 0.40)',
 		textShadowOffset: {width: -1, height: 1},
 		textShadowRadius: 20
+	},
+
+	userIcon: {
+		height:'35%'
+	},
+
+	signInButton: {
+		width: 312, 
+		height: 48
 	}
 });
 
