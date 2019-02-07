02cfd9b8ae5458a9395d1848675040f03fa3cbd1
Benoît Jeaurond <jeaurond.benoit@gmail.com>
2019-02-07 13:00:25 -0500

    Added gradient animation

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 export const webClientId = "359972006564-9tngmtaigit300v7kqjr48sph3apvq38.apps.googleusercontent.com";
-
 export const googleIdentityScope = ["profile", "https://www.googleapis.com/auth/calendar"];
