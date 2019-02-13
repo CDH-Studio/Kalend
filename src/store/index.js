513eabcc9b5a47e1ea70565f99b391124351726c
Fahad Hayat <fahadhayat@outlook.com>
2019-02-13 14:30:14 -0500

    created redux store

---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+
+export default store = createStore(rootReducer);
+
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import thunk from 'redux-thunk';
+// import reducer from '../reducers';
+// import { createLogger } from 'redux-logger';
+
+// const log = createLogger({ diff: true, collapsed: true});
+
+// export default(initialState = {}) => {
+
+// 	const middlerware = [thunk, log];
+// 	const enhancers = [];
+
+// 	const store = createStore(
+// 		reducer(),
+// 		initialState,
+// 		compose(
+// 			applyMiddleware(...middlerware,...enhancers)
+// 		)
+// 	);
+
+// 	return store;
+// };
