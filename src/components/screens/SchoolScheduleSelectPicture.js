736aae4797d20cd4c47fe961529c4fbe8c2fd8f8
Benoit Jeaurond <bjeau025@uottawa.ca>
2019-02-15 12:31:04 -0500

    Reverted one line of code

--- src/components/screens/SchoolScheduleSelectPicture.js
+++ src/components/screens/SchoolScheduleSelectPicture.js
@@ -201,7 +201,7 @@ class SchoolScheduleSelectPicture extends React.Component {
 
 							<View style={styles.imageGrid}>
 
-								{ this.state.images.map((image, index) => {
+								{ images.map((image, index) => {
 									return (
 										<CameraRollImage key={index}
 											image={image} 
