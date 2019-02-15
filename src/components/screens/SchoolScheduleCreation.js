df3bac57c4f24aba2c415dfac724ee2b896e793a
Benoit Jeaurond <bjeau025@uottawa.ca>
2019-02-15 13:12:06 -0500

    Started working on loading screen

--- src/components/screens/SchoolScheduleCreation.js
+++ src/components/screens/SchoolScheduleCreation.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ImageBackground, StatusBar, Platform, StyleSheet, View, Dimensions} from 'react-native';
 import { analyzePicture } from '../../services/service';
+import { gradientColors } from '../../../config';
+import LinearGradient from 'react-native-linear-gradient';
+import { ProgressBar, Colors, Surface } from 'react-native-paper';
 
 class SchoolScheduleCreation extends React.Component {
+	static navigationOptions = {
+		title: 'Analysing Schedule',
+		headerTintColor: '#fff',
+		headerTitleStyle: {
+			fontFamily: 'Raleway-Regular'
+		},
+		headerTransparent: true,
+		headerStyle: {
+			backgroundColor: 'rgba(0, 0, 0, 0.2)',
+			marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+		}
+	};
 
 	componentDidMount() {
 		analyzePicture();
@@ -10,9 +25,41 @@ class SchoolScheduleCreation extends React.Component {
 
 	render() {
 		return(
-			<Text>It works!</Text>
+			<LinearGradient style={styles.container} colors={gradientColors}>
+				<ImageBackground style={styles.container} source={require('../../assets/img/loginScreen/backPattern.png')} resizeMode="repeat">
+					<StatusBar translucent={true} backgroundColor={'rgba(0, 0, 0, 0.4)'} />
+
+
+ 				 <Surface style={styles.surface}>
+
+						<ProgressBar style={{height: 10}}color={Colors.red800} />
+		
+					</Surface>
+				</ImageBackground>
+			</LinearGradient>
 		);
 	}
 }
+
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: '100%',
+		height: '130%' //Fixes pattern bug
+	},
+
+	surface: {
+		padding: 8,
+		height: 80,
+		width: Dimensions.get('window').width * 0.8,
+		borderRadius: 4,
+		justifyContent: 'center',
+		elevation: 3,
+	},
+});
 
 export default SchoolScheduleCreation;
