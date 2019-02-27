de3cd9cc9d42b2397f52ae96a39791d2aa61a0fe
MarieEveLegault <marie_legault_@hotmail.com>
2019-02-27 14:47:38 -0500

    Update EditSchoolSchedule.js

--- src/components/screens/EditSchoolSchedule.js
+++ src/components/screens/EditSchoolSchedule.js
@@ -1,7 +1,52 @@
 import React from 'react';
-import {StatusBar, Text, View} from 'react-native';
+import {Platform, StatusBar, Text, View} from 'react-native';
+import { blueColor } from '../../../config';
 
 class EditSchoolSchedule extends React.Component {
+
+	// Style for Navigation Bar
+	static navigationOptions = {
+		title: 'Edit Course',
+		headerTintColor: 'white',
+		headerTitleStyle: {fontFamily: 'Raleway-Regular'},
+		headerTransparent: true,
+		headerStyle: {
+			backgroundColor: blueColor,
+			marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+		}
+	};
+
+	// Constructor and States
+	constructor(props) {
+		super(props);
+		this.state = { 
+			//Height of Screen
+			containerHeight: null,
+
+			//Title of Event
+			courseCode: '',
+			
+			//Time section
+			startDate: new Date().toDateString(),
+			minStartDate: new Date().toDateString(),
+			maxStartDate: new Date(8640000000000000),
+
+			startTime: new Date().toLocaleTimeString(),
+			disabledStartTime : false,
+			amPmStart: this.getAmPm(),
+
+			endTime: new Date().toLocaleTimeString(),
+			minEndTime: new Date().toLocaleTimeString(),
+			disabledEndTime : true,
+			amPmEnd: this.getAmPm(),
+
+			//Other Information
+			location: '',
+			recurrenceValue: 'None',
+			recurrence: 'NONE'
+		};
+	}
+
 	render() {
 		return(
 			<View style={{width: '100%', height: '100%', paddingTop:100}}>
