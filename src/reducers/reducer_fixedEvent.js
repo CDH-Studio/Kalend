529aa32824278cceb7663348e7510cbf1ead1299
Fahad Hayat <fahadhayat@outlook.com>
2019-02-28 16:33:02 -0500

    Fixed the Course Reducer

--- src/reducers/reducer_fixedEvent.js
+++ src/reducers/reducer_fixedEvent.js
@@ -1,12 +1,10 @@
-import {ADD_FE, ADD_COURSE} from '../constants';
+import {ADD_FE} from '../constants';
 
 export default function FixedEventsReducer(state = [], action) {
 	const { event } = action;
 	console.log(state);
 	switch (action.type) {
 		case ADD_FE:
-			return  [...state, event];
-		case ADD_COURSE: 
 			return  [...state, event];
 
 		default:
