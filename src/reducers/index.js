2248b69f9b150b61f131a9f70e72d1be35b168e3
Fahad Hayat <fahadhayat@outlook.com>
2019-02-28 10:35:38 -0500

    added redcuers to index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import HomeReducer from './reducer_user';
 import ImageReducer  from './reducer_image';
+import FixedEventsReducer from './reducer_fixedEvent';
+import NonFixedEventsReducer from './reducer_nonfixedEvent';
 
-export default combineReducers( { HomeReducer, ImageReducer });
+export default combineReducers( { HomeReducer, ImageReducer, FixedEventsReducer, NonFixedEventsReducer });
