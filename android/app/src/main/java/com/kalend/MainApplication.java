9146146e7b3366f1736c2e1fefa75754d431219a
MarieEveLegault <marie_legault_@hotmail.com>
2019-03-26 13:38:53 -0400

    Link Slider and ViewPager.

--- android/app/src/main/java/com/kalend/MainApplication.java
+++ android/app/src/main/java/com/kalend/MainApplication.java
@@ -3,6 +3,8 @@ package com.kalend;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
+import com.reactnativecommunity.slider.ReactSliderPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
@@ -36,6 +38,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCViewPagerPackage(),
+            new ReactSliderPackage(),
             new AsyncStoragePackage(),
 		       new RNFirebasePackage(),
           new LottiePackage(),
