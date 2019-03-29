273a0239c4f3e2aa8aaaa545bbea25b584785d69
cuaim <fahadhayat@outlook.com>
2019-03-29 15:34:25 -0400

    Fixed manual addition of course error

--- src/components/screens/Course.js
+++ src/components/screens/Course.js
@@ -10,6 +10,7 @@ import { updateCourses, addCourse } from '../../actions';
 import BottomButtons from '../BottomButtons';
 import { CourseRoute, SchoolScheduleRoute, DashboardNavigator, ReviewEventRoute, SchoolInformationRoute } from '../../constants/screenNames';
 import updateNavigation from '../NavigationHelper';
+import { getStartDate } from '../../services/helper';
 import { courseStyles as styles, statusBlueColor, gray, dark_blue, blue, white } from '../../styles';
 
 const moment = require('moment');
@@ -42,18 +43,13 @@ class Course extends React.PureComponent {
 
 		this.state = { 
 			containerHeight,
-
-			summary: '',
-
 			dayOfWeek: 'Monday',
 			dayOfWeekValue: 'Monday',
-
 			startTime: moment().format('h:mm A'),
-
 			endTime: moment().format('h:mm A'),
 			minEndTime: moment().format('h:mm A'),
 			disabledEndTime: true,
-
+			summary: '',
 			location: ''
 		};
 
@@ -266,25 +262,23 @@ class Course extends React.PureComponent {
 			return false;
 		}
 
-		// gets the next weekday date
-		let date = this.getNextWeekdayDate();
+		let courseStartDate = getStartDate(this.props.semesterStartDate, this.state.dayOfWeek);
+		let courseEndDate = getStartDate(this.props.semesterStartDate, this.state.dayOfWeek);
 
-		let endtime = new Date(date.getTime());
-		endtime = this.getDateFromTimeString(this.state.endTime, endtime);
-		endtime = endtime.toJSON();
+		courseEndDate = this.getDateFromTimeString(this.state.endTime, courseEndDate);
+		courseEndDate = courseEndDate.toJSON();
 
-		let starttime = new Date(date.getTime());
-		starttime = this.getDateFromTimeString(this.state.startTime, starttime);
-		starttime = starttime.toJSON();
+		courseStartDate = this.getDateFromTimeString(this.state.startTime, courseStartDate);
+		courseStartDate = courseStartDate.toJSON();
 
 		return this.setState({
 			end: {
 				timeZone: 'America/Toronto',
-				dateTime: endtime
+				dateTime: courseEndDate
 			},
 			start: {
 				timeZone: 'America/Toronto',
-				dateTime: starttime
+				dateTime: courseStartDate
 			}
 		}, () => {
 			this.props.dispatch(addCourse(this.state));
@@ -310,17 +304,11 @@ class Course extends React.PureComponent {
 	}
 
 	getDateFromTimeString = (timeString, currentDate) => {
-		if (currentDate === undefined) {
-			currentDate = new Date();
-		}
-
 		// cleans up the time in the state
 		let info = timeString.split(' ').map(i => i.split(':'));
 		currentDate.setHours( 
 			parseInt(info[0][0]) + (info[1][0] === 'AM' ? 0 : 12), 
-			parseInt(info[0][1]), 
-			0,
-			0);
+			parseInt(info[0][1]));
 			
 		return currentDate;
 	}
@@ -332,23 +320,18 @@ class Course extends React.PureComponent {
 		this.setState({
 			summary: '',
 			courseCodeValidated: true,
-			
 			dayOfWeek: 'Monday',
 			dayOfWeekValue: 'Monday',
-
 			startTime: moment().format('h:mm A'),
-
 			endTime: moment().format('h:mm A'),
 			minEndTime: moment().format('h:mm A'),
 			disabledEndTime: true,
 			endTimeValidated: true,
-
 			location: '',
 			snackbarVisible: false,
 			snackbarText: '',
 			snackbarTime: 3000,
-
-			recurrence: 'RRULE:FREQ=WEEKLY;UNTIL=20190327'
+			recurrence: [`RRULE:FREQ=WEEKLY;UNTIL=${this.props.semesterEndDate};`]
 		});
 	}
 
@@ -550,13 +533,18 @@ class Course extends React.PureComponent {
 }
 
 let mapStateToProps = (state) => {
-	const { CoursesReducer, NavigationReducer } = state;
+	const { CoursesReducer, NavigationReducer, SchoolInformationReducer } = state;
 	let selected = NavigationReducer.reviewEventSelected;
+	let semesterEndDate = new Date(SchoolInformationReducer.info.info.endDate);
+	let semesterStartDate = new Date(SchoolInformationReducer.info.info.startDate);
+	semesterEndDate  = semesterEndDate.toISOString().split('T')[0].replace(/-/g, '');
 
 	return {
 		CourseState: CoursesReducer[selected],
 		CoursesReducer,
-		selectedIndex: selected
+		selectedIndex: selected,
+		semesterEndDate,
+		semesterStartDate
 	};
 };
 
