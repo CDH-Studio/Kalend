1c509b73a083f6610113558e820c09858b02c0cc
Benoit Jeaurond <bjeau025@uottawa.ca>
2019-04-03 07:47:21 -0400

    Linked new library

--- android/app/src/main/java/com/kalend/MainApplication.java
+++ android/app/src/main/java/com/kalend/MainApplication.java
@@ -23,6 +23,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.RNFetchBlob.RNFetchBlobPackage;   
 
 import java.util.Arrays;
 import java.util.List;
@@ -55,7 +56,8 @@ public class MainApplication extends Application implements ReactApplication {
           new RNFirebaseMessagingPackage(),
           new RNFirebaseAnalyticsPackage(),
           new RNFirebaseCrashlyticsPackage(),
-          new RNFirebasePerformancePackage()
+          new RNFirebasePerformancePackage(),
+          new RNFetchBlobPackage()
       );
     }
 
