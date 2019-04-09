f4ab2ffdc3cb3d522e319ff4325922814d95768f
cuaim <fahadhayat@outlook.com>
2019-04-09 09:32:34 -0400

    Created api helper calls for server's database

---
+++ src/services/api/storage_services.js
@@ -0,0 +1,55 @@
+let server  =  'http://172.17.73.9:8080/';
+
+let apiHelperCall = (URL, method, data) => {
+
+	let fetchData = {
+		method,
+		credentials: 'same-origin',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+	};
+
+	if( method == 'POST') fetchData.body = JSON.stringify(data);
+
+	return fetch(URL, fetchData);
+};
+
+export const storeUserInfoService = (info) => {
+	console.log('storeUserInfoService');
+	return apiHelperCall(server + 'api/logUser','POST', info);
+};
+
+export const getUserInfoService = () => {
+	return apiHelperCall(server + 'api/logUser','GET');
+};
+
+export const updateUser = (info) => {
+	return apiHelperCall(server + 'api/updateUser','POST', info);
+};
+
+export const storeSchoolInfoService = (info) => {
+	return apiHelperCall(server + 'api/storeSchoolInfo','POST', info);
+};
+
+export const getSchoolInfoService = () => {
+	return apiHelperCall(server + 'api/getSchoolInfo','GET');
+};
+
+export const storeUserHours = (info) => {
+	return apiHelperCall(server + 'api/storeUserHours', 'POST', info);
+};
+
+export const getUserHours = () => {
+	return apiHelperCall(server + 'api/storeUserHours', 'GET');
+};
+
+export const storeGeneratedCalendars = (info) => {
+	return apiHelperCall(server + 'api/storeGeneratedCalendars', 'POST', info);
+};
+
+export const getEvents = () => {
+	console.log('getEvents');
+	return apiHelperCall(server + 'api/getEvents', 'GET');
+};
